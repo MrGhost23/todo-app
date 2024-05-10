0b10f17ee03aa35e49674e86033dd6f4feeaa8b2
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 20:24:33 +0300

    using .env with configmodule

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+  });
+  
   await app.listen(5000);
 }
 bootstrap();
