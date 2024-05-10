b3015a0da2b9ebefe67f5f74c3ae939738889397
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 21:44:08 +0300

    added: delete, update, toggleComplete

--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, UseGuards, Request, Delete, Put } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { TaskService } from './task.service';
@@ -23,5 +23,23 @@ export class TaskController {
   @Get(':taskId')
   async findOne(@Request() req, @Param('taskId') taskId: string) {
     return this.taskService.findOne(req.user._id,taskId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.taskService.delete(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateTaskDto: CreateTaskDto) {
+    return this.taskService.update(id, updateTaskDto.title, updateTaskDto.dueDate, updateTaskDto.isCompleted);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id/toggleComplete')
+  async toggleComplete(@Param('id') id: string) {
+    return this.taskService.toggleComplete(id);
   }
 }
