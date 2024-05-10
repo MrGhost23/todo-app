6e0b6502b7c9aee966f0502fd42d729e38be71dd
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 20:23:20 +0300

    login & register controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/dtos/create-user.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { LocalStrategy } from './local-auth.guard';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @UseGuards(LocalStrategy)
+    @Post('login')
+    async login(@Body() user: LoginDto) {
+      return this.authService.login(user);
+    }
+  
+    @Post('register')
+    async register(@Body() createUserDto: CreateUserDto) {
+      return this.authService.register(createUserDto);
+    }
+    
+    @UseGuards(JwtAuthGuard)
+    @Post('validate')
+    validate(@Req() req) {
+      const userObject = req.user.toObject();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...userWithoutPassword } = userObject;
+      return userWithoutPassword;
+    }
+}
