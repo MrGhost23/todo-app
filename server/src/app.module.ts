ab91ffc0ae7a52f1021e1474211f78abd3381459
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 20:22:38 +0300

    adding more imports in module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    UserModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
