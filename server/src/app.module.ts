77253299fe4f95417d127348d62ca5bb0659a3e3
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 21:33:53 +0300

    added: TaskModule in imports

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
-    
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
