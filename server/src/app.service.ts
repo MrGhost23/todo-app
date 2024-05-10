5569ba916a8dec6c1a07a60a33e018e17df2c84c
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 20:22:24 +0300

    making sure of connection with mongodb

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  private readonly logger = new Logger(AppService.name);
+
+  constructor(@InjectConnection() private connection: Connection) {}
+
+  async onModuleInit() {
+    try {
+      await this.connection.db.command({ ping: 1 });
+      this.logger.log('Connected to MongoDB');
+    } catch (error) {
+      this.logger.error('Error connecting to MongoDB', error.stack);
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
