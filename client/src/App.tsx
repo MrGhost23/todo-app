562e4b4479884d43bc58af2f60b1bcf3d1526c23
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-10 20:36:05 +0300

    added: react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,39 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Tasks />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
 function App() {
   return (
     <div>
-      <h1>App</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
