b9d1306f7c7250da7345303bf8899af85d623777
Omar Mohamed <72778139+MrGhost23@users.noreply.github.com>
2024-05-11 05:26:27 +0300

    finished tasks page except handling error

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -1,4 +1,44 @@
+import TaskStat from "@/components/TaskStat";
+import TasksList from "@/components/Tasks/TasksList";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import TasksSkeletons from "@/components/ui/skeletons/TasksSkeletons";
+import { TaskType } from "@/types/Task";
+import { fetchTasks } from "@/utils/taskActions";
+import { useQuery } from "@tanstack/react-query";
+
 const Tasks = () => {
-  return <div>Tasks</div>;
+  const {
+    data: tasks,
+    isLoading,
+    isError,
+  } = useQuery<TaskType[]>({
+    queryKey: ["tasks"],
+    queryFn: fetchTasks,
+  });
+
+  const totalTasks = tasks?.length ?? 0;
+  const completedTasks = tasks?.filter((task) => task.isCompleted).length ?? 0;
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+        <div className="grid lg:col-span-3">
+          <TaskStat totalTasks={totalTasks} completedTasks={completedTasks} />
+        </div>
+        <div className="grid lg:col-span-9">
+          <div className="flex flex-col">
+            <p className="text-2xl mb-4">All Tasks</p>
+            <Separator className="mb-4" />
+          </div>
+          {isLoading ? (
+            <TasksSkeletons number={6} />
+          ) : (
+            <TasksList tasks={tasks ?? []} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Tasks;
